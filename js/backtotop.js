0a8474ef7d4da5f1c25667026d65e6591534a9ed
NavhLuny <90888088+NavhLuny@users.noreply.github.com>
2021-12-15 23:29:15 +0700

    Add files via upload

---
+++ js/backtotop.js
@@ -0,0 +1,115 @@
+//back to top
+//BACK TO TOP
+// File#: _1_back-to-top
+// Usage: codyhouse.co/license
+(function() {
+  var backTop = document.getElementsByClassName('js-back-to-top')[0];
+  if( backTop ) {
+    var dataElement = backTop.getAttribute('data-element');
+    var scrollElement = dataElement ? document.querySelector(dataElement) : window;
+    var scrollDuration = parseInt(backTop.getAttribute('data-duration')) || 300, //scroll to top duration
+      scrollOffsetInit = parseInt(backTop.getAttribute('data-offset-in')) || parseInt(backTop.getAttribute('data-offset')) || 0, //show back-to-top if scrolling > scrollOffset
+      scrollOffsetOutInit = parseInt(backTop.getAttribute('data-offset-out')) || 0, 
+      scrollOffset = 0,
+      scrollOffsetOut = 0,
+      scrolling = false;
+
+    // check if target-in/target-out have been set
+    var targetIn = backTop.getAttribute('data-target-in') ? document.querySelector(backTop.getAttribute('data-target-in')) : false,
+      targetOut = backTop.getAttribute('data-target-out') ? document.querySelector(backTop.getAttribute('data-target-out')) : false;
+
+    updateOffsets();
+    
+    //detect click on back-to-top link
+    backTop.addEventListener('click', function(event) {
+      event.preventDefault();
+      if(!window.requestAnimationFrame) {
+        scrollElement.scrollTo(0, 0);
+      } else {
+        dataElement ? Util.scrollTo(0, scrollDuration, false, scrollElement) : Util.scrollTo(0, scrollDuration);
+      } 
+      //move the focus to the #top-element - don't break keyboard navigation
+      Util.moveFocus(document.getElementById(backTop.getAttribute('href').replace('#', '')));
+    });
+    
+    //listen to the window scroll and update back-to-top visibility
+    checkBackToTop();
+    if (scrollOffset > 0 || scrollOffsetOut > 0) {
+      scrollElement.addEventListener("scroll", function(event) {
+        if( !scrolling ) {
+          scrolling = true;
+          (!window.requestAnimationFrame) ? setTimeout(function(){checkBackToTop();}, 250) : window.requestAnimationFrame(checkBackToTop);
+        }
+      });
+    }
+
+    function checkBackToTop() {
+      updateOffsets();
+      var windowTop = scrollElement.scrollTop || document.documentElement.scrollTop;
+      if(!dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
+      var condition =  windowTop >= scrollOffset;
+      if(scrollOffsetOut > 0) {
+        condition = (windowTop >= scrollOffset) && (window.innerHeight + windowTop < scrollOffsetOut);
+      }
+      Util.toggleClass(backTop, 'back-to-top--is-visible', condition);
+      scrolling = false;
+    }
+
+    function updateOffsets() {
+      scrollOffset = getOffset(targetIn, scrollOffsetInit, true);
+      scrollOffsetOut = getOffset(targetOut, scrollOffsetOutInit);
+    }
+
+    function getOffset(target, startOffset, bool) {
+      var offset = 0;
+      if(target) {
+        var windowTop = scrollElement.scrollTop || document.documentElement.scrollTop;
+        if(!dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
+        var boundingClientRect = target.getBoundingClientRect();
+        offset = bool ? boundingClientRect.bottom : boundingClientRect.top;
+        offset = offset + windowTop;
+      }
+      if(startOffset && startOffset) {
+        offset = offset + parseInt(startOffset);
+      }
+      return offset;
+    }
+  }
+}());
+
+
+//accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+//smooth scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
